a0eb793a5352559dc51a51554f56a78ef2a1f0f3
MamonkinaS <32412548+MamonkinaS@users.noreply.github.com>
2017-12-09 19:38:04 +0300

    Модернизация скрипта под БД

--- project_22.py
+++ project_22.py
@@ -2,42 +2,55 @@ import pickle
 import datetime
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
+import sqlite3
+import datetime
 
+path2 = 'base.db'
+
+def select(db_name, sql):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.execute(sql)
+    rez = cursor.fetchall()    
+    conn.close()
+    return rez
 
 def str_to_int(string):
-    pr = rezult[i]['count_of_viewers']
+    pr = rezult[i][3]
     string = string.replace(' руб.', '')
     string = string.split(' ')
     string = string[0].replace('\xa0', '')
     pr = int(string)
-    #print(pr)
+   #print(pr)
     return(pr)
 
 
 if __name__ == "__main__":
     with open('rezult.picle', 'rb') as f:
         rezult = pickle.load(f)
-
-       
+        
+    sql="""SELECT * FROM weeks"""
+    rezult = select(path2, sql)
+    print(rezult[0])
     price = []
     count = []
     aver_price = []
     date = []
     for i in range(len(rezult)):
         index = len(rezult) - i - 1
-        pr = str_to_int(rezult[index]['count_of_viewers'])
+        pr = str_to_int(rezult[index][3])
         count.append(pr)
-        pr = str_to_int(rezult[index]['summ_total'])
+        pr = str_to_int(rezult[index][2])
         price.append(pr)
         aver_price.append(price[-1]/count[-1])
-        date.append(datetime.datetime(int(rezult[index]['year']),
-                  int(rezult[index]['month']), int(rezult[index]['day'])))
-        
-        
-        
+        date.append(datetime.datetime.strptime(rezult[index][1], '%Y-%m-%d'))
+        #date.append(datetime.datetime(int(rezult[index]['year']),
+        #           int(rezult[index]['month']), int(rezult[index]['day'])))
+         
+         
+         
     res = seasonal_decompose(price, model='multiplicative', freq=12*4)
     
-    
+     
     plt.plot(date, price, label='сборы за уик-энд')
     plt.legend(loc='upper left')
     plt.show()
